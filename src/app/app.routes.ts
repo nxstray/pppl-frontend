9e67a7a63d97c8c9a7d80ec659c68d384a2c46b2
nxstray <afwanapriansyah@gmail.com>
2025-11-29 21:09:13 +0700

    done: added new endpoint

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./pages/landing/landing.component').then(m => LandingComponent)
-  }
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
+  },
 ];
