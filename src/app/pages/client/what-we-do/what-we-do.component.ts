589f22019e3f4e9dc63f4f9b7c30c659b958a037
Muhamad Raka Pratama <raka824pratama@gmail.com>
2026-01-04 00:44:16 +0700

    done: new c ft.UI what-we-do

---
+++ src/app/pages/client/what-we-do/what-we-do.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-what-we-do',
+  imports: [],
+  templateUrl: './what-we-do.component.html',
+  styleUrl: './what-we-do.component.scss'
+})
+export class WhatWeDoComponent {
+
+}
