018056d3e6d13da82968fc350f0ca90c59de5e6c
nxstray <afwanapriansyah@gmail.com>
2026-02-07 09:12:13 +0700

    chore: inject vercel speed insight for performance

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { register } from 'swiper/element/bundle';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 register();
+injectSpeedInsights();
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));  
